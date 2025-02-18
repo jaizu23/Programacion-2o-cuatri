e98161379afdd6265d909a267bdbc04cc0c8b508
jaizu23 <157589155+jaizu23@users.noreply.github.com>
2025-02-18 18:24:19 +0100

    Python files added

---
+++ Python/ejercicios/ejercicio6.py
@@ -0,0 +1,43 @@
+
+def esPerfecto(num: int) -> bool:
+    """O(n)"""
+    i = 1
+    div = []
+    def sumaLista(lista: list) -> int:
+        sum = 0
+        for i in lista:
+            sum += i
+        return sum
+    if num < 0:
+        return False
+    while i < num:
+        if num % i == 0:
+            div.append(i)
+        i += 1
+    return sumaLista(div) == num
+
+
+
+##############################
+#          TESTS             #
+##############################
+
+def test_comprobar_perfecto_8128(benchmark):
+    resultado = benchmark(esPerfecto,8128)
+    assert resultado == True
+
+def test_comprobar_perfecto_1000(benchmark):
+    resultado = benchmark(esPerfecto,1000)
+    assert resultado == False
+
+def test_casos_limite_1():
+    resultado = esPerfecto(-6)
+    assert resultado == False
+
+def test_casos_limite_2():
+    resultado = esPerfecto(0)
+    assert resultado == True
+
+def test_casos_limite_3():
+    resultado = esPerfecto(6)
+    assert resultado == True
