4582a0df6b2288008699a6ef78aee4bf56de54aa
jaizu23 <157589155+jaizu23@users.noreply.github.com>
2025-02-27 17:57:02 +0100

    Fixed python folder

---
+++ ejercicios/ejercicio5.py
@@ -0,0 +1,39 @@
+from math import sqrt
+
+
+def esPrimo (num: int) -> bool:
+    """O(sqrt(n))"""
+    i = 2
+    if num <= 0:
+        return False
+    while i <= sqrt(num):
+        if num % i == 0:
+            return False
+        i+=1
+    return True
+        
+
+
+##############################
+#          TESTS             #
+##############################
+
+def test_comprobar_primo_1000(benchmark):
+    resultado = benchmark(esPrimo,1000)
+    assert resultado == False
+
+def test_comprobar_primo_3863(benchmark):
+    resultado = benchmark(esPrimo,3863)
+    assert resultado == True
+
+def test_5_casos_limite_1():
+    resultado = esPrimo(-1)
+    assert resultado == False
+
+def test_5_casos_limite_2():
+    resultado = esPrimo(0)
+    assert resultado == False
+
+def test_5_casos_limite_3():
+    resultado = esPrimo(2)
+    assert resultado == True
---
+++ ejercicios/test_all.py
@@ -0,0 +1,3 @@
+from ejercicio5 import *
+from ejercicio6 import *
+from ejercicio9 import *
