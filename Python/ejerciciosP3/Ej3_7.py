edc12baa8075db8d2e255126e338809b1b702252
LucasMarcosVaquerizo <157593171+LucasMarcosVaquerizo@users.noreply.github.com>
2025-03-27 19:57:18 +0100

    test Ej3_9 terminado

--- Python/ejerciciosP3/Ej3_7.py
+++ Python/ejerciciosP3/Ej3_7.py
@@ -43,3 +43,45 @@ matriz = [
     [1, 0, 1, 0, 0]
 ]
 print(traspuesta(matriz))
+
+
+##############################
+#          TESTS             #
+##############################
+
+
+def test_traspuesta():
+    Entrada = [[
+    [0, 0, 0, 1, 0], 
+    [0, 0, 1, 0, 1], 
+    [1, 1, 0, 0, 0], 
+    [1, 1, 0, 1, 1], 
+    [1, 0, 1, 0, 0]
+],[
+    [1, 2, 3], 
+    [4, 5, 6], 
+    [7, 8, 9], 
+    [10, 11, 12]
+],[
+    [5, 6], 
+    [7, 8], 
+    [9, 10]
+]]
+    valores = [[
+    [0, 0, 1, 1, 1], 
+    [0, 0, 1, 1, 0], 
+    [0, 1, 0, 0, 1], 
+    [1, 0, 0, 1, 0], 
+    [0, 1, 0, 1, 0]
+],[
+    [1, 4, 7, 10], 
+    [2, 5, 8, 11], 
+    [3, 6, 9, 12]
+],[
+    [5, 7, 9], 
+    [6, 8, 10]
+]]
+    resultado = [] # Almacena el valor que devuelve traspuesta()
+    for matriz in Entrada:
+        resultado.append(traspuesta(matriz))
+    assert resultado == valores, 'Las matrices no coinciden' # Comprueva que el valor coincide
